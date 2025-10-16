63cce4834fac9468cc685e7f24ccd1ba610da5c1
Bar <barle34@gmail.com>
2025-10-17 01:03:40 +0300

    finished_day01_part1

--- 2024/day01.py
+++ 2024/day01.py
@@ -1,1 +1,19 @@
-pass
+with open("2024/inputs/day01_input") as myfile:
+    day1_input = myfile.readlines()
+
+left_list = []
+right_list = []
+
+for i in day1_input:
+    pair = i.split()
+    left_list.append(int(pair[0]))
+    right_list.append(int(pair[1]))
+
+left_list.sort()
+right_list.sort()
+diff = 0
+
+for i in range(len(day1_input)):
+    diff += abs(left_list[i] - right_list[i])
+
+print(diff)
