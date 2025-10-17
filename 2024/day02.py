0374e398bb96e941197a26e481e4942cbc55d380
Bar <barle34@gmail.com>
2025-10-18 01:34:15 +0300

    day02_part2_wip

--- 2024/day02.py
+++ 2024/day02.py
@@ -5,29 +5,65 @@ path = os.path.split(__file__)[0]
 with open(f"{path}/inputs/{day}_input") as myfile:
     input = myfile.readlines()
 
-safe = 0
-for report in input:
-    report = report.split()
-    report = list(map(int, report))
-    ascending = False
-    descending = False
-    valid = True
-    for idx, _ in enumerate(report):
-        if idx == 0:
+def part1():
+    safe = 0
+    for report in input:
+        report = report.split()
+        report = list(map(int, report))
+        ascending = False
+        descending = False
+        valid = True
+        for idx, _ in enumerate(report):
+            if idx == 0:
+                continue
+            if abs(report[idx] - report[idx-1]) > 3:
+                valid = False
+                break
+            if (report[idx] > report[idx-1]):
+                ascending = True
+            elif (report[idx] < report[idx-1]):
+                descending = True
+            else: # not ascending or descending
+                valid = False
+                break
+        if not valid:
             continue
-        if abs(report[idx] - report[idx-1]) > 3:
-            valid = False
-            break
-        if (report[idx] > report[idx-1]):
-            ascending = True
-        elif (report[idx] < report[idx-1]):
-            descending = True
-        else: # not ascending or descending
-            valid = False
-            break
-    if not valid:
-        continue
-    if ascending != descending:
-        safe += 1
+        if ascending != descending:
+            safe += 1
 
-print(safe)
+    print(safe)
+
+def part2():
+    safe = 0
+    for report in input:
+        report = report.split()
+        report = list(map(int, report))
+        ascending = False
+        descending = False
+        valid = True
+        faults = 0
+        for idx, _ in enumerate(report):
+            if idx == 0:
+                continue
+            if (abs(report[idx] - report[idx-1]) > 3):
+                if faults:
+                    valid = False
+                    break
+                faults += 1
+            if (report[idx] > report[idx-1]):
+                ascending = True
+            elif (report[idx] < report[idx-1]):
+                descending = True
+            else: # not ascending or descending
+                if faults:
+                    valid = False
+                    break
+                faults += 1
+        if faults > 1:
+            valid = False
+        if not valid:
+            continue
+        if ascending != descending:
+            safe += 1
+
+    print(safe)
